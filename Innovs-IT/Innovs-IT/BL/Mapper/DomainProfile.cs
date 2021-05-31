67085a7e42686702e67c7ab94aaf38b2652d9dcd
Abdelrahman Othman <abdalrahmanosmandokhan@yahoo.com>
2021-05-31 18:12:41 +0200

    First Commit

---
+++ Innovs-IT/Innovs-IT/BL/Mapper/DomainProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Innovs_IT.BL.Mapper
+{
+    public class DomainProfile: Profile
+    {
+        public DomainProfile()
+        {
+            // CreateMap<Employee, EmployeeVM>();
+            //CreateMap<EmployeeVM, Employee>();
+
+
+        }
+    }
+}
---
+++ Innovs-IT/Innovs-IT/DAL/Database/DbContainer.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Innovs_IT.DAL.Database
+{
+    public class DbContainer : IdentityDbContext
+    {
+        public DbContainer(DbContextOptions<DbContainer> opts) : base(opts) { }
+
+
+        
+        
+
+    }
+}
